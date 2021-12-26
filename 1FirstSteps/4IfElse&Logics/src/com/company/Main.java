3f1d710ce8e8246e7a39e0ea48e60cb342a9cfb6
Tanmay Garg <79107818+TanmayGarg001@users.noreply.github.com>
2021-12-26 19:54:39 +0530

    Add files via upload

---
+++ 1FirstSteps/4IfElse&Logics/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("18+ go ahead!");
+        }
+        int marks = 35;
+        boolean isPass = (marks >= 33) ? true : false;
+        System.out.println(isPass);
+    }
+}
