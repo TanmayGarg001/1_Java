d5b858e1fadc6cdbd774028196f4aa39ba0fa99e
Tanmay Garg <79107818+TanmayGarg001@users.noreply.github.com>
2022-01-24 19:41:04 +0530

    Add files via upload

---
+++ 8NamingConventionsPackagesStatic&FinalKeywords/0NamingConventions/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //It is good practice to follow some good naming conventions rule. So that its easier to read for others and for yourself too.
+        //Let's say you see your written code after a month ,So it would be easy to read for you if you follow naming conventions.
+        //Because you will forget eventually why you did something at certain spot, So we can make life easier
+        //of ours and for other programmers if we follow these naming conventions.
+
+        //Things we will name in java are:
+        //Packages
+        //Classes
+        //Interfaces
+        //Methods
+        //Constants
+        //Variables
+        //Type Parameters
+        //See the images all these are really important to remember and follow as they will make you a better programmer.
+        //there are 2 typos in slides:-
+        //SydneySwans should've been sydneySwans
+        //BoxLength should've been boxLength
+    }
+}
