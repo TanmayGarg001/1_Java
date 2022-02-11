a3da04dd90ae6581f79f785c113f2b889dc01f0e
Tanmay Garg <79107818+TanmayGarg001@users.noreply.github.com>
2022-02-11 14:45:36 +0530

    Add files via upload

---
+++ 0.0CodingExcercises/2MegaBytesConverter/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(2500);
+        printMegaBytesAndKiloBytes(-1024);
+        printMegaBytesAndKiloBytes(5000);
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kiloBytes + " KB" + " = " + kiloBytes / 1024 + " MB and " + kiloBytes % 1024 + " KB");
+        }
+    }
+}
