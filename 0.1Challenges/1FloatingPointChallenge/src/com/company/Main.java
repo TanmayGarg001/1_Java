d3007d5bb68e0fb999d4735110e3697977fa51c3
Tanmay Garg <79107818+TanmayGarg001@users.noreply.github.com>
2022-02-15 08:56:20 +0530

    Add files via upload

---
+++ 0.1Challenges/1FloatingPointChallenge/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double pound = 8d;
+        double kilograms = pound * 0.45359237;
+        System.out.println(kilograms);
+    }
+}
