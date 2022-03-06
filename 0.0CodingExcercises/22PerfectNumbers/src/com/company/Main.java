c7918114e8da43128cf0508050c4b9bf02e7e15f
Tanmay Garg <79107818+TanmayGarg001@users.noreply.github.com>
2022-03-06 23:57:54 +0530

    Add files via upload

---
+++ 0.0CodingExcercises/22PerfectNumbers/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(-5));
+        System.out.println(isPerfectNumber(1));
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            return false;
+        } else {
+            int sum = 0;
+            for (int i = 1; i < number; i++) {
+                if (number % i == 0) {
+                    sum = sum + i;
+                }
+            }
+            return sum == number;
+        }
+    }
+}
