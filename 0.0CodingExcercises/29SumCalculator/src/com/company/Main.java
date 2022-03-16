5d501a7bf07166ae895053ef2f3d23ee4c222b3e
Tanmay Garg <79107818+TanmayGarg001@users.noreply.github.com>
2022-03-16 14:50:18 +0530

    Add files via upload

---
+++ 0.0CodingExcercises/29SumCalculator/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println(calculator.getAdditionResult());
+        System.out.println(calculator.getSubtractionResult());
+
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println(calculator.getMultiplicationResult());
+        System.out.println(calculator.getDivisionResult());
+    }
+}
